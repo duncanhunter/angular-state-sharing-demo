b7291f1111a750d29f2de3309e94cd272b8d44f7
Duncan Hunter <duncan@duncanhunter.com.au>
2017-08-24 11:13:10 +1000

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pal-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'pal';
+}
